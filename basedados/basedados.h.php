46a4c38e7a04a0d3d3b70ae7274befabd9b3c957
M0rdBugs <146108844+M0rdBugs@users.noreply.github.com>
2026-03-06 04:57:19 +0000

    Beta
    Versão não testada na base de dados, versão inicial do index com tema geral realizado

---
+++ basedados/basedados.h.php
@@ -0,0 +1,15 @@
+<?php
+    class Database {
+        private static $conn;
+
+        public static function connect() {
+            if (!self::$conn) {
+                self::$conn = new mysqli("localhost", "root", "", "trabalho1PHP");
+                if (self::$conn->connect_error) {
+                    die("Conecção falhou: " . self::$conn->connect_error);
+                }
+            }
+            return self::$conn;
+        }
+    }
+?>
---
+++ basedados/criar_bd.sql
@@ -0,0 +1,33 @@
+-- Criar banco de dados
+CREATE DATABASE IF NOT EXISTS trabalho1php 
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE trabalho1php;
+
+-- Tabela utilizador
+CREATE TABLE IF NOT EXISTS utilizador (
+    utilizador_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    password VARCHAR(255),
+    tipo_util ENUM('visitante', 'cliente', 'funcionario', 'administrador') NOT NULL DEFAULT 'visitante',
+    saldo DECIMAL(10,2) DEFAULT 0.00
+);
+
+-- Tabela transacoes
+CREATE TABLE IF NOT EXISTS transacoes (
+    transacoes_id INT PRIMARY KEY AUTO_INCREMENT ,
+    user_id INT NOT NULL,
+    valor DECIMAL(10,2) NOT NULL,
+    tipo ENUM('CARGA', 'COMPRA', 'REEMBOLSO') NOT NULL,
+    data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES utilizador(utilizador_id)
+);
+
+-- Inserir dados 
+INSERT INTO utilizador (nome, email, password, tipo_util, saldo) VALUES
+    ('visitante', NULL, NULL, 'visitante', 0.00), 
+    ('cliente', 'cliente@email.com', 'cliente', 'cliente', 100.00),
+    ('funcionário', 'funcionario@empresa.com', 'funcionario', 'funcionario', 0.00),
+    ('admin', 'admin@empresa.com', 'admin', 'administrador', 0.00);
+  
